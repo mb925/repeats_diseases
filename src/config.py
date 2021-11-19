66d8274e88aad9d5f8547cc29c5f4135f49c0a1e
mb925 <martina.bevilacqua5@libero.it>
2021-11-19 14:34:49 +0100

    initial databases

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os
+
+absolute = os.path.abspath(os.getcwd())
+absolute = absolute + '/../'
+
+data = {
+    "trp_diseases": absolute + 'data/trp_diseases'
+}
---
+++ src/parse.py
@@ -0,0 +1,72 @@
+import sys
+import config as cfg
+import pandas as pd
+import math
+
+
+def main():
+
+    table_count()
+
+
+def how_many(elements):
+    list_pdb = []
+    for uniprot in elements:
+        if isinstance(uniprot, str):
+            for pdb in uniprot.split(';'):
+                if pdb not in list_pdb and pdb != '':
+                    list_pdb.append(pdb)
+    return len(list_pdb)
+
+def table_count():
+    df_uniprots = pd.read_csv(cfg.data['trp_diseases'] + '/uniprot_type_disease.tsv', sep='\t')
+    df_repeats = pd.read_csv(cfg.data['trp_diseases'] + '/repeatsdb_type_disease.tsv', sep='\t')
+    # todo df_trp_dis = df_repeats[[df_repeats['Involvement in disease'].notna()]]
+
+
+    # TRP
+    # filter
+    df_pdb_trp = df_repeats['Cross-reference (PDB)'].tolist()
+    df_pdb_trp_human = df_repeats.loc[(df_repeats.Organism == 'Homo sapiens (Human)')]['Cross-reference (PDB)'].tolist()
+    # count
+    pdb_trp = how_many(df_pdb_trp)
+    pdb_human_trp = how_many(df_pdb_trp_human)
+    uniprot_trp = len(df_repeats['Entry'].index)
+    uniprot_human_trp = len(df_repeats.loc[(df_repeats.Organism == 'Homo sapiens (Human)')]['Entry'].index)
+
+
+    # DISEASE
+    # filter
+    df_pdb_disease = df_uniprots['Cross-reference (PDB)'].tolist()
+    pdb_human_disease = df_uniprots.loc[(df_uniprots.Organism == 'Homo sapiens (Human)')]['Cross-reference (PDB)'].tolist()
+    # count
+    pdb_disease = how_many(df_pdb_disease)
+    pdb_human_disease = how_many(pdb_human_disease)
+    uniprot_disease = len(df_uniprots['Entry'].index)
+    uniprot_human_disease = len(df_uniprots.loc[(df_uniprots.Organism == 'Homo sapiens (Human)')]['Entry'].index)
+
+    # TRP & DISEASE
+    # filter
+    # filter
+    # todo
+    # pdb_trp_dis = df_repeats['Cross-reference (PDB)'].tolist()
+    # pdb_human_trp_dis = df_repeats.loc[(df_repeats.Organism == 'Homo sapiens (Human)')]['Cross-reference (PDB)'].tolist()
+    # # count
+    # pdb_trp_dis = how_many(df_pdb_trp)
+    # pdb_human_trp_dis = how_many(df_pdb_trp_human)
+    # uniprot_trp_dis = len(df_repeats['Entry'].index)
+    # uniprot_human_trp_dis = len(df_repeats.loc[(df_repeats.Organism == 'Homo sapiens (Human)')]['Entry'].index)
+
+
+    rows = [(pdb_trp, pdb_human_trp, uniprot_trp, uniprot_human_trp),
+            (pdb_disease, pdb_human_disease, uniprot_disease, uniprot_human_disease),
+            (pdb_trp_dis, pdb_human_trp_dis, uniprot_trp_dis, uniprot_human_trp_dis)]
+    df_count_disease = pd.DataFrame(rows, columns=['PDB', 'PDB Human', 'UniProt', 'UniProt Human'], index=['TRP', 'disease', 'TRP & disease'])
+    print(df_count_disease)
+
+
+    # df_repeats.to_csv(cfg.data['trp_diseases'] + '/count_disease.tsv', sep='\t', index=False)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
